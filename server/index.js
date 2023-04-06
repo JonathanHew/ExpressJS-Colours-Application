9da0e486359877a2372825c4090a7ca21f4fea00
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-06 11:58:44 +0100

    implemented GET /colors/id/ which returns the details of the color with the corresponding id

--- server/index.js
+++ server/index.js
@@ -16,9 +16,18 @@ app.listen(5004, () => {
 });
 
 //routes
+
 //gets the list of all colours and their details
 app.get("/colors", async (req, res) => {
   res.json({
-    colors: colors
+    colors
+  });
+});
+//gets the details of colour with colorId of id
+app.get("/colors/:id", (req, res) => {
+  const id = req.params.id;
+  const color = colors.find((c) => (c.colorId == id));
+  res.json({
+    color
   });
 });

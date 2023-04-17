21a7a84097c3d15474983e450de5a61f6e3246e7
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-17 13:48:31 +0100

    Background color now changes when Set button is clicked

--- client/src/Components/SetBackground.js
+++ client/src/Components/SetBackground.js
@@ -3,7 +3,13 @@ import React from "react";
 const SetBackground = ({ colors, index }) => {
   const onSet = (e) => {
     console.log(colors[index].hexString);
+    setBackgroundColor(colors[index].hexString)
   };
+
+  function setBackgroundColor(hexString) {
+    document.body.style.backgroundColor = hexString;
+  }
+  
   return (
     <div>
       <button className="btn btn-primary" onClick={(e) => onSet(e)}>

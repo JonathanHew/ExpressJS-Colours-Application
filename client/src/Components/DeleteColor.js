6e11bae3928ace09f918cc1cac6eb3f451a54d6a
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2023-04-17 11:43:44 +0100

    implemented axios request to delete highlighted color

--- client/src/Components/DeleteColor.js
+++ client/src/Components/DeleteColor.js
@@ -1,13 +1,19 @@
 import React from "react";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const DeleteColor = ({ index, colors }) => {
-  const onDelete = (e) => {
+
+  const onDelete = async(e) => {
     e.preventDefault();
     console.log(index);
     const colorId = colors[index].colorId;
-
     try {
-        
+        await axios
+        .delete(`http://localhost:5004/colors/${colorId}`, {})
+        .then((res) => {
+          console.log(res);
+        })
     } catch (err) {
         console.error(err.message);
     }
